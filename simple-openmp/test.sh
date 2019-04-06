97eef8c2301f44d46f6a8f716fe565be506367ec
pingjui <pingjuiliao@gmail.com>
2019-04-06 14:21:08 -0700

    first commit

---
+++ simple-openmp/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# number of threads:
+for t in 1 2 4 6 8 ; do
+    echo NUMT = $t
+    # number of subdivisions:
+    for s in 2 4 8 16 32 64 128 256 512 1024 2048 3072 4096 ; do
+        echo NUMS = $s
+        g++ -DNUMS=$s -DNUMT=$t proj.cpp -o proj -lm -fopenmp
+        ./proj
+    done
+done
